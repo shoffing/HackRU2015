86b41af31ff032d679eccfad7be4f61ae5a9febb
shoffing <shoffing@gmail.com>
2015-10-03 21:22:38 -0400

    fix gitignore

---
+++ HackRU2015/Assets/Scripts/Blood.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blood : MonoBehaviour {
+
+	void Start() {
+        StartCoroutine(WaitAndDie());
+    }
+
+    IEnumerator WaitAndDie() {
+        yield return new WaitForSeconds(2);
+        //Destroy(gameObject);
+    }
+
+}
